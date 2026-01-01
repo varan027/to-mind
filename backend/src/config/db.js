224687d2fdf006daa566a8e026351faf3819af2b
varan027 <varanlucky02@gmail.com>
2026-01-01 16:06:42 +0530

    deployment issues

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -6,6 +6,5 @@ export const connectDB = async () => {
     console.log('MongoDB connected successfully');  
   } catch (error) {
     console.error('MongoDB connection failed:', error.message);
-    process.exit(1);
   }
 }
--- backend/src/middleware/rateLimiter.js
+++ backend/src/middleware/rateLimiter.js
@@ -1,17 +1,21 @@
 import ratelimit from "../config/upstash.js";
 
 const rateLimiter = async (_, res, next) => {
-  try {
-    const { success } = await ratelimit.limit("my-ratelimit");
-    if (!success)
-      return res
-        .status(429)
-        .json({ message: "Too many requests, Try again later" });
+  if (process.env.NODE_ENV === "false") {
+    try {
+      const { success } = await ratelimit.limit("my-ratelimit");
+      if (!success)
+        return res
+          .status(429)
+          .json({ message: "Too many requests, Try again later" });
 
+      next();
+    } catch (error) {
+      console.log("Rate Limiter Error:", error);
+      next(error);
+    }
+  } else {
     next();
-  } catch (error) {
-    console.log("Rate Limiter Error:", error);
-    next(error);  
   }
 };
 
