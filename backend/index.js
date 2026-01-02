687f565fb46360fe40648186dafc469226093b79
varan027 <varanlucky02@gmail.com>
2026-01-02 12:22:09 +0530

    fix:db connect

--- backend/index.js
+++ backend/index.js
@@ -1,45 +1,29 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import path from "path";
 
 import notesRoutes from "./src/routes/notesRoutes.js";
 import { connectDB } from "./src/config/db.js";
-// import rateLimiter from "./src/middleware/rateLimiter.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-// const __dirname = path.resolve();
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://to-mind.vercel.app"
+  "https://tomind-git-main-varan027s-projects.vercel.app/"
 ];
 
-if (process.env.NODE_ENV !== "production") {
   app.use(
     cors({
       origin: allowedOrigins,
     })
   );
-}
 
 app.use(express.json());
-// // Enable rate limiting ONLY in production
-// if (process.env.NODE_ENV === "production") {
-//   app.use(rateLimiter);
-// }
 
-app.use("/", notesRoutes);
-
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-//   });
-// }
+app.use("/api", notesRoutes);
 
 app.get("/health", (req, res) => {
   res.status(200).send("OK");
