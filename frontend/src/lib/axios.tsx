8d81efa7feef121104fd373ca3f83e5b1b9039bc
varan027 <varanlucky02@gmail.com>
2026-01-02 12:13:31 +0530

    fix: deploy crash

--- frontend/src/lib/axios.tsx
+++ frontend/src/lib/axios.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: "https://tomind.onrender.com/",
   headers: {
     "Content-Type": "application/json",
   },
