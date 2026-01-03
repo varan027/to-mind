20fe6dca4deba03ccdac7406a08e5b74bbd922e4
varan027 <varanlucky02@gmail.com>
2026-01-03 19:59:28 +0530

    authroute /me to restoreuser

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,7 +1,12 @@
 import express from "express";
 import { login, register } from "../controllers/authController.js";
+import { authProtect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
+
+router.get("/me", authProtect, (req, res) => {
+  res.json(req.user);
+});
 
 router.post("/register", register)
 router.post("/login", login);
