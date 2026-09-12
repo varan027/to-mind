f5dc091601bf723ba1e5723df6225ad031a1b56c
varan027 <varanlucky02@gmail.com>
2026-09-12 23:30:47 +0530

    fix: local dns code removed

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,7 +1,4 @@
 import mongoose from "mongoose";
-import dns from "dns";
-
-dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 export const connectDB = async () => {
   const mongoUri = process.env.MONGO_URI;
